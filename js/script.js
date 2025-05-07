173a9b26bc0e42d3896692623edb14260da30129
Kitler174 <mateuszblaszczyk36@gmail.com>
2025-05-07 19:33:52 +0200

    pozycje

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     const text = '';
     let i = 0;
     const element = document.getElementById('text');
@@ -15,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
     setTimeout(() => {
         element.style.border = "none"
     }, 5000);
+    
 });
 
 function setCookie(name, value) {
@@ -67,3 +69,43 @@ function openb(inde,th){
         lastopb = 0;
     }
 }
+
+// Funkcja pomocnicza do opóźnienia
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Zdobądź wszystkie elementy .el na stronie
+    const elements = document.querySelectorAll('.el');
+
+    elements.forEach(el => {
+        const img = el.querySelector('img');
+        const h1 = el.querySelector('h1');
+        const p = el.querySelector('p');
+
+        img.style.top = (h1.offsetHeight + p.offsetHeight + 5) + 'px';  // Dodajemy wysokość h1 i p do top
+    });
+
+    // Iteruj po każdym elemencie .el
+    elements.forEach(el => {
+        const p = el.querySelector('p');
+        const h1 = el.querySelector('h1');
+        const img = el.querySelector('img');
+
+        // Po najechaniu na element .el
+        el.addEventListener('mouseenter', async () => {
+            p.style.opacity = '0';
+            h1.style.opacity = '0';
+            img.style.marginTop = -(h1.offsetHeight + p.offsetHeight) + 'px'; // Ustawienie marginesu górnego obrazu
+        });
+
+        // Po wyjściu z .el – przywróć widoczność elementów
+        el.addEventListener('mouseleave', async () => {
+            // Przywróć widoczność tekstów
+            h1.style.opacity = '1';
+            p.style.opacity = '1';
+            img.style.marginTop = '0';  // Resetowanie marginTop
+        });
+    });
+});
